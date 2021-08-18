8ce2ecd39ee1f0e5e22cd300ed15439c980c3126
enikebraimoh <richardbraimoh@gmail.com>
2021-08-18 01:05:28 +0100

    Done major task

--- app/src/main/java/com/zuri/hng/i4g/hng_task_2/MainActivity.kt
+++ app/src/main/java/com/zuri/hng/i4g/hng_task_2/MainActivity.kt
@@ -2,10 +2,32 @@ package com.zuri.hng.i4g.hng_task_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.core.widget.addTextChangedListener
+import com.zuri.hng.i4g.hng_task_2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.input.addTextChangedListener(object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+               binding.output.text = s
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        })
+        setContentView(binding.root)
     }
 }
