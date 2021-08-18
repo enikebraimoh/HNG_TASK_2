7441e4f3cef3cb79635783ea2ccce117ccf33650
enikebraimoh <richardbraimoh@gmail.com>
2021-08-18 04:47:39 +0100

    Done With main task

--- app/src/main/java/com/zuri/hng/i4g/hng_task_2/MainActivity.kt
+++ app/src/main/java/com/zuri/hng/i4g/hng_task_2/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.zuri.hng.i4g.hng_task_2
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -28,6 +30,20 @@ class MainActivity : AppCompatActivity() {
 
             }
         })
+
+        binding.button.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://hng.tech/"))
+            startActivity(i)
+        }
+        binding.button2.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://ingressive.org"))
+            startActivity(i)
+        }
+        binding.button3.setOnClickListener {
+            val i = Intent(Intent.ACTION_VIEW, Uri.parse("https://training.zuri.team"))
+            startActivity(i)
+        }
+
         setContentView(binding.root)
     }
 }
